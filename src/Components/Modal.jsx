2c2d8d104c0a811c25e1c3ed64d0fbcb09a471ae
spd99 <89846672+ToniSh69@users.noreply.github.com>
2023-03-28 19:25:53 +0200

    small fixes

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Modal(props) {
   return (
     <>
-      <div className=" flex max-w-sm flex-col gap-3 rounded-lg bg-white px-5 py-6">
+      <div className=" mx-4 flex max-w-sm flex-col gap-3 rounded-lg bg-white px-5 py-6">
         <h2 className="text-xl font-semibold">Delete comment</h2>
         <p className="text-grayish-blue">
           Are you sure you want to delete this comment? This will remove the
@@ -11,13 +11,13 @@ export default function Modal(props) {
         </p>
         <div className="flex justify-between">
           <button
-            className=" rounded-md bg-grayish-blue py-2 px-6 uppercase text-white"
+            className=" rounded-md bg-grayish-blue py-2 px-6 uppercase text-white hover:opacity-75"
             onClick={() => props.handleIsDeleting()}
           >
             No, Cancel
           </button>
           <button
-            className="rounded-md bg-soft-red py-2 px-6 uppercase text-white"
+            className="rounded-md bg-soft-red py-2 px-6 uppercase text-white hover:opacity-75"
             onClick={() => props.deleteComment()}
           >
             Yes, Delete
